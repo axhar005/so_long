53eb3c7fde164d60e7e1b2fdea79002fbcaab1ed
Olivier Boucher <oboucher@c3r5p6.42quebec.com>
2023-07-24 13:39:36 -0400

    add menu texture

--- src/free.c
+++ src/free.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   free.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: olivierboucher <olivierboucher@student.    +#+  +:+       +#+        */
+/*   By: oboucher <oboucher@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/17 14:46:45 by olivierbouc       #+#    #+#             */
-/*   Updated: 2023/07/24 08:35:43 by olivierbouc      ###   ########.fr       */
+/*   Updated: 2023/07/24 13:33:43 by oboucher         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,21 +24,21 @@ void    **ft_sfree_2d(void **ptr)
     return(NULL);
 }
 
-// void    free_map(void)
-// {
-//     int x;
-//     int y;
+void    free_map(void)
+{
+    int x;
+    int y;
     
-//     x = 0;
-//     while (g()->map[x])
-//     {
-//         y = 0;
-//         while (g()->map[x][y])
-//         {
-//             ft_sfree(g()->map[x][y]);
-//             y++;
-//         }
-//         x++;
-//     }
+    x = 0;
+    while (g()->map[x])
+    {
+        y = 0;
+        while (g()->map[x][y])
+        {
+            ft_sfree(g()->map[x][y]);
+            y++;
+        }
+        x++;
+    }
     
-// }
+}
